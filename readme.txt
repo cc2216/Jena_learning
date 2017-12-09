ed805260e8a5ce00b73c9eb85a5c130ffea24dff
cc2216 <chch2216@gmail.com>
2017-12-09 01:14:32 +0100

    update project of jena_rules to allow adding more than one rules file, added readme.txt to show how to run this project

---
+++ readme.txt
@@ -0,0 +1,9 @@
+This project is for implementing query of family tree by rules added.
+It allow users to add more rules file for query of family tree ontology.
+
+For using this project, just run it as java application and follow the screen prompt.
+The rules files and ontology file should be added in the owl folder.
+
+The reference of the rules files family.rules and family2.rules is http://blog.csdn.net/yangchang999/article/details/6281779
+The reference of the family tree ontology is http://www.voidcn.com/article/p-cfoonkdd-bdh.html
+
--- src/main/java/com/jenamaven/jenarules/JenaRules.java
+++ src/main/java/com/jenamaven/jenarules/JenaRules.java
@@ -1,7 +1,11 @@
 package com.jenamaven.jenarules;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
@@ -23,38 +27,94 @@ public class JenaRules {
 	Model data;
 	Reasoner reasoner;
 	InfModel infmodel;
+	private static Scanner sc;
 	
 	public static void main(String[] args) {		
 		// TODO Auto-generated method stub
 		JenaRules familyTree = new JenaRules();
-
-		Resource name = familyTree.findResource("#Lily");
-		Property op = familyTree.setProperty(null);
-		System.out.println("lily has relatives:");
 		
-		familyTree.printStatements(name, op, null);
+		try {
+			while(true){
+				int option = readOption();
+				if (option == 2) {
+					break;
+				} else if (option == 1) {
+					System.out.println("Please input the name of rules file:"); 
+					String fileName = readString();
+					familyTree.addRulesFile(fileName);
+				} else {
+					System.out.println("Option is wrong!");
+				}
+			}	
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			System.out.println("Please input the name of the person:");  
+            String namePerson = readString();
+            System.out.println("Please input the the relationship you want to query (if you want query all relatives, please entre \" all \":");
+            String relationship = readString();
+            Resource name = familyTree.findResource(namePerson); //set up the name of person
+            Property op = familyTree.setProperty(relationship == null? null:(relationship)); // set up the relationship 
+            System.out.println(namePerson + " " + (relationship == null? "all relatives are" : relationship) + ":");
+            familyTree.printStatements(name, op, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }	
 		
 	}
+	
+	private static String readString() throws IOException {
+        String userInput;  
+        sc=new Scanner(System.in);
+        userInput=sc.next();
+         System.out.println(userInput);
+         if (userInput.equals("all")) {
+        	 	return null;
+        	 } else {
+        		 return userInput;
+        	 }
+    }
+	
+	private static int readOption() throws IOException {
+        int option;  
+        sc = new Scanner(System.in);  
+        System.out.println("If you want add new rules please input \'1\', else input \'2\' to query the family tree:");  
+        option=sc.nextInt();  
+        return option;
+    }
 	
 	public JenaRules(){
 		dataFile = JenaRules.class.getClassLoader().getResource("family.owl").getPath(); //path of family owl
 		familyRules = JenaRules.class.getClassLoader().getResource("family.rules").getPath();//path of family rules
-		prefix = "http://www.semanticweb.org/ontologies/2010/0/family.owl"; //main namespace
+		String familyRules2 = JenaRules.class.getClassLoader().getResource("family2.rules").getPath();//path of family rules
+		prefix = "http://www.semanticweb.org/ontologies/2010/0/family.owl#"; //main namespace
 		
 		//setup the reasoner with rules
 		this.rules = Rule.rulesFromURL(familyRules);
 		this.data = FileManager.get().loadModel(dataFile);
-		this.reasoner = new GenericRuleReasoner(rules);
+		this.reasoner = new GenericRuleReasoner(this.rules);
 		this.infmodel = ModelFactory.createInfModel(reasoner, data);
 		this.infmodel.removeNsPrefix(this.prefix);
 		
 	};
 	
+	public void addRulesFile(String fileName) {
+		//Add new rules file		
+		String newFamilyRules = JenaRules.class.getClassLoader().getResource(fileName).getPath();//path of family rules
+		List<Rule> newRules = Rule.rulesFromURL(newFamilyRules);
+		Reasoner newReasoner = new GenericRuleReasoner(newRules);
+		this.infmodel = ModelFactory.createInfModel(newReasoner, infmodel);
+		this.infmodel.removeNsPrefix(this.prefix);
+	}
+	
 	public void printStatements(Resource s, Property p, Resource o) {
 	    for (StmtIterator i = this.infmodel.listStatements(s,p,o); i.hasNext(); ) {
 	        Statement stmt = i.nextStatement();
 	        System.out.println(" - " + PrintUtil.print(stmt));
-	        System.out.println("--" + stmt.getPredicate());
+	        System.out.println("--" + stmt.getObject());
 	    }
 	}
 	
@@ -68,7 +128,12 @@ public class JenaRules {
 	}
 	
 	public Resource findResource(String name) {
-		return this.infmodel.getResource(this.prefix + name);
+		if (name != null) {
+			return this.infmodel.getResource(this.prefix + name);
+		} else {
+			return null;
+		}
+		
 	}
 	
 	public void validation() {
